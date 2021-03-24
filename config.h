95a87bd7a1bb38baa089601bf4d3dcdb17ef852d
Athul P <athul929@gmail.com>
2021-03-24 16:17:04 +0530

    apply titlecolor patch

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMonoMedium Nerd Font:size=10" };
+static const char *fonts[]          = { "JetBrainsMonoMedium Nerd Font:size=10", "Noto Color Emoji" };
 static const char dmenufont[]       = "JetBrainsMonoMedium Nerd Font:size=10";
 static const char col_gray1[]       = "#282c34";
 static const char col_gray2[]       = "#444444";
@@ -14,8 +14,9 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#61afef";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeTitle]  = { col_gray4, col_gray1,  col_cyan  },
 };
 
 /* tagging */
@@ -93,7 +94,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
 };
 
 /* button definitions */
